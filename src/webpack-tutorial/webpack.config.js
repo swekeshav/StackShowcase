23a7a9f71c8b08cadb8ad2507c9f2c2c49aee3f1
Keshav Sharma <sharma.keshav39@gmail.com>
2025-08-18 21:22:19 +0530

    Minification of the resulting webpack bundle using Terser plugin

--- src/webpack-tutorial/webpack.config.js
+++ src/webpack-tutorial/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -46,5 +47,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new TerserPlugin()
+  ],
 };
