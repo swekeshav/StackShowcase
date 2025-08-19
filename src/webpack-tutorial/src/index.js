2dd1782f9b6db469cdeff7fcfe07e76271a7c120
Keshav Sharma <sharma.keshav39@gmail.com>
2025-08-19 16:37:34 +0530

    Extracting CSS Into a Separate Bundle, Part 2

--- src/webpack-tutorial/src/index.js
+++ src/webpack-tutorial/src/index.js
@@ -1,9 +1,13 @@
 import helloWorld from "./hello-world";
-import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";  
+import HelloWorldButton from "./components/hello-world-button/hello-world-button.js"; 
+import Heading from "./components/heading/heading.js"; 
 import addImage from "./add-image";
 
 helloWorld();
 addImage();
+
+const heading = new Heading();
+heading.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
