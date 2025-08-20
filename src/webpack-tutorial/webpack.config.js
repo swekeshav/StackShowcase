0f875ab403abd0315cd43a740224547ea70021e9
Keshav Sharma <sharma.keshav39@gmail.com>
2025-08-20 14:59:39 +0530

    Add html-webpack-plugin for generating html with contentHash version of js/ css files

--- src/webpack-tutorial/webpack.config.js
+++ src/webpack-tutorial/webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    publicPath: "",
     // clean: {
     //   dry: true,
     //   keep: /\.css$/
@@ -64,6 +65,7 @@ module.exports = {
       //   '**/*',
       //   path.join(process.cwd(), 'build/**/*')
       // ]
-    })
+    }),
+    new HtmlWebpackPlugin()
   ],
 };
