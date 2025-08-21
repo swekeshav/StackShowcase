e6845d6c1b9b487540b6922d58db5a34a97c7764
Keshav Sharma <sharma.keshav39@gmail.com>
2025-08-21 11:56:12 +0530

    Clean up configurations

--- src/webpack-tutorial/src/index.js
+++ src/webpack-tutorial/src/index.js
@@ -8,8 +8,6 @@ addImage();
 
 const heading = new Heading();
 heading.render();
-const heading2 = new Heading();
-heading2.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
