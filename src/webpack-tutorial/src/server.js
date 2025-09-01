bcaafda5204e8baf8a549850e963fe22b53fb4ab
Keshav Sharma <sharma.keshav39@gmail.com>
2025-09-01 18:57:12 +0530

    Serving HTML pages via Express

--- src/webpack-tutorial/src/server.js
+++ src/webpack-tutorial/src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const pathToHTMLFile = path.resolve(__dirname, '../dist/hello-world.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHTMLFile, 'utf-8');
+  res.send(contentFromHtmlFile);    
 });
 
 app.listen(3000, () => {
