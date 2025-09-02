4137b4652fcd37d317a4028896ec7eb7f236f6f6
Keshav Sharma <sharma.keshav39@gmail.com>
2025-09-02 14:16:03 +0530

    154/v1 Understand Webpack Integration with Node and Express (#155)
    * Integrating Express into our Application
    
    * Serving HTML pages via Express
    
    * Handling JS and CSS via Express
    
    * Integrating Express.js into a multiple page application

--- src/webpack-tutorial/src/hello-world.js
+++ src/webpack-tutorial/src/hello-world.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading.js"; 
 import HelloWorldButton from "./components/hello-world-button/hello-world-button.js"; 
-import React from "react";
 
 const heading = new Heading();
 heading.render('Hello World');
