63eb60f319e61d33bc6b62d94b622377a43d0823
Keshav Sharma <sharma.keshav39@gmail.com>
2026-01-12 19:47:42 +0530

    feat: add image upload functionality with preview in profile pic uploader

--- src/porfolio-projects/profile-pic-uploader/src/main.js
+++ src/porfolio-projects/profile-pic-uploader/src/main.js
@@ -2,3 +2,15 @@ const btnUpload = document.getElementById('btnUpload')
 btnUpload.addEventListener('click', () => {
     console.log('A picture is uploaded!')
 })
+
+const uploadImage = document.getElementById('uploadImage')
+uploadImage.addEventListener('change', () => {
+    console.log('Image file selected:', uploadImage.files[0])
+    const file = uploadImage.files[0]
+    const reader = new FileReader()
+    reader.onload = function (e) {
+        const uploadPreview = document.getElementById('uploadPreview')
+        uploadPreview.src = e.target.result
+    }
+    reader.readAsDataURL(file)
+})
