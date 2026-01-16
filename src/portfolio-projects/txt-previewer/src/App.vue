3e5f8b0db394439743a307d415f3c37e7e1866ef
Keshav Sharma <sharma.keshav39@gmail.com>
2026-01-16 18:32:35 +0530

    feat: move file reading/ previewing functionality to vue sfc

---
+++ src/portfolio-projects/txt-previewer/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Text Previewer</h1>
+    <TxtPreviewer></TxtPreviewer>
+</template>
+
+<script setup>
+import TxtPreviewer from './components/txt-previewer/TxtPreviewer.vue'
+</script>
---
+++ src/portfolio-projects/txt-previewer/src/components/txt-previewer/TxtPreviewer.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="file" accept=".txt,text/plain" @change="uploadTextFile" />
+    <pre id="previewTextFile">{{ fileContent }}</pre>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    let fileContent = ref('')
+    const uploadTextFile = () => {
+        const uploadFile = document.querySelector('input[type="file"]')
+       var fileReader = new FileReader()
+        fileReader.readAsText(uploadFile.files[0])
+        fileReader.onload = function (e) {
+            fileContent.value = e.target.result
+        }
+    }
+</script>
