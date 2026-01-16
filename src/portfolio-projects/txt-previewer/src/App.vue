d704c02521ca872e55cce214d2cc6725cb5904d4
Keshav Sharma <sharma.keshav39@gmail.com>
2026-01-16 18:56:52 +0530

    192/v1 Convert txt-previewer app to use vue & vite (#195)
    * Refactor txt-previewer structure and add Vue dependencies
    
    * feat: move file reading/ previewing functionality to vue sfc
    
    * refactor: restructure App.vue and TxtPreviewer.vue for improved layout and word count display

---
+++ src/portfolio-projects/txt-previewer/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <header>
+            <h1>Text Previewer</h1>
+        </header>
+        <section>
+            <TxtPreviewer />
+        </section>
+    </main>
+</template>
+
+<script setup>
+import TxtPreviewer from './components/txt-previewer/TxtPreviewer.vue'
+</script>
---
+++ src/portfolio-projects/txt-previewer/src/components/txt-previewer/TxtPreviewer.vue
@@ -0,0 +1,31 @@
+<template>
+    <input type="file" accept=".txt,text/plain" @change="uploadTextFile" />
+    <pre v-if="fileContent">
+        <strong>
+This article contains {{ wordCount }} words.
+        </strong>
+    </pre>
+    <pre v-if="fileContent" id="previewTextFile">{{ fileContent }}</pre>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    const fileContent = $ref('')
+    let wordCount = $ref(0)
+
+    const uploadTextFile = (event: Event) => {
+        const files = event.target.files
+        if(files.length === 0) {
+            console.log('No file selected')
+            return
+        }
+        
+        const fileReader = new FileReader()
+        fileReader.readAsText(files[0])
+        fileReader.onload = function (e) {
+            fileContent = e.target.result
+            wordCount = fileContent.split(' ').length + 1
+        }
+    }
+</script>
