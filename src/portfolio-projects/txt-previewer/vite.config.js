44bb05a5427f1bcfdd2a37b0c514dd848f8e77b5
Keshav Sharma <sharma.keshav39@gmail.com>
2026-01-21 16:09:26 +0530

    198/v1: Remove reactivityTransform changes (#199)

--- src/portfolio-projects/txt-previewer/vite.config.js
+++ src/portfolio-projects/txt-previewer/vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
-    plugins: [vue({
-        reactivityTransform: true
-    })],
+    plugins: [vue()],
     base: "",
     build: {
         assetsDir: "./"
