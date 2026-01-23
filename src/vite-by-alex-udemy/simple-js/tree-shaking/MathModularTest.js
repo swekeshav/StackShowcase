7d06512f489b71d8626d897494471aa0984159b0
Keshav Sharma <sharma.keshav39@gmail.com>
2026-01-23 19:30:13 +0530

    207/v1 Understand Tree Shaking concept using different examples (#210)
    * feat: add Vite configuration and update README with CJS/ESM module comparison
    
    * chore: move age display functionality to a new folder
    
    * feat: implement modular and non-modular math functions with corresponding tests

---
+++ src/vite-by-alex-udemy/simple-js/tree-shaking/MathModularTest.js
@@ -0,0 +1,3 @@
+import { squared } from './MathModular.js'
+
+console.log(squared(5)) // Output: 25
---
+++ src/vite-by-alex-udemy/simple-js/tree-shaking/MathNonModular.js
@@ -0,0 +1,8 @@
+const coolMath = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+}
+
+export default coolMath
